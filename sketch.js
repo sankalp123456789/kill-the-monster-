c2fdfa2ef43f0d8b987fc2778ae97457d1b45c3e
sankalp123456789 <73840145+sankalp123456789@users.noreply.github.com>
2021-09-13 17:29:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,96 +1,67 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
-
-var engine, world;
-var box1, box2, box3,box4,box5,box6,box7,box8,box9,box10,box11,box12,box13,box14,box15,box16,box17,box18,box19,box20,box21,box22,box23,box24,box25,box26;
-var hero,monster,rope,ground;
-
-function preload() {
-  bg = loadImage("background.png");
-}
-
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+var divisions =[];
+var divisionHeight=300;
+var score =0;
 function setup() {
-  createCanvas(1400, 800);
+  createCanvas(800, 800);
   engine = Engine.create();
   world = engine.world;
+  ground = new Ground(width/2,height,width,20);
 
-  ground = new Ground(600, 600, 1200, 20);
+  //create division objects
+  for (var k = 0; k <=800; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
 
-  hero = new Hero(400,800,250);
-  rope = new Rope(hero.body, { x: 500, y: 50 });
-  monster = new Monster(1100,550,300);
+  //create 1st row of plinko objects
+  for (var j = 75; j <=width; j=j+50) { 
+    plinkos.push(new Plinko(j,75));
+  }
 
-  box1 = new Box(600, 200, 70, 70);
-  box2 = new Box(900, 200, 70, 70);
-  box3 = new Box(900, 200, 70, 70);
-  box4 = new Box(900, 200, 70, 70);
-  box5 = new Box(900, 200, 70, 70);
-  box6 = new Box(900, 200, 70, 70);
-  box7 = new Box(800, 200, 70, 70);
-  box8 = new Box(800, 200, 70, 70);
-  box9 = new Box(800, 100, 70, 70);
-  box10 = new Box(800, 200, 70, 70);
-  box11 = new Box(800, 200, 70, 70);
-  box12 = new Box(800, 200, 70, 70);
-  box13 = new Box(800, 100, 70, 70);
-  box14 = new Box(800, 200, 70, 70);
-  box15 = new Box(700, 200, 70, 70);
-  box16 = new Box(700, 200, 70, 70);
-  box17 = new Box(700, 100, 70, 70);
-  box18 = new Box(700, 100, 70, 70);
-  box19 = new Box(700, 100, 70, 70);
-  box20 = new Box(700, 100, 70, 70);
-  box21 = new Box(600, 100, 70, 70);
-  box22 = new Box(600, 100, 70, 70);
-  box23 = new Box(600, 100, 70, 70);
-  box24 = new Box(600, 100, 70, 70);
-  box25 = new Box(600, 100, 70, 70);
-  box26 = new Box(600, 100, 70, 70);
+  //create 2nd row of plinko objects
+  for (var j = 50; j <=width-10; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,175));
+  }
 
+  //create 3rd row of plinko objects
+  
+  
+  //create 4th row of plinko objects
+  
+
+    
 }
+ 
+
 
 function draw() {
-  background(bg);
+  background("black");
+  textSize(20)
+ 
   Engine.update(engine);
   ground.display();
-  box1.display();
-  box2.display();
-  box3.display();
-  box4.display()
-  box5.display()
-  box6.display()
-  box7.display()
-  box8.display()
-  box9.display()
-  box10.display()
-  box11.display()
-  box12.display()
-  box13.display()
-  box14.display()
-  box15.display()
-  box16.display()
-  box17.display()
-  box18.display()
-  box19.display()
-  box20.display()
-  box21.display()
-  box22.display()
-  box23.display()
-  box24.display()
-  box25.display()
-  box26.display()
+  
+  //display the plinkos 
+  for (var i = 0; i < plinkos.length; i++) {
+    plinkos[i].display();   
+  }
+   
+  //display the divisions
+  for (var k = 0; k < divisions.length; k++) {
+    divisions[k].display();
+  }
 
-  hero.display();
-  rope.display();
-  monster.display();
+  //create the particles using frameCount
+
+
+  //display the particles 
+
 
 }
-
-function mouseDragged() {
-  //set the position of hero when mouse is dragged
-  Matter.Body.setPosition(hero.body, { x:mouseX, y: mouse});
-}
-
-
